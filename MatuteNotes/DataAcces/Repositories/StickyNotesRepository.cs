e5926059e75f1ee921614f52034e9d0c5c1eff8f
Carrizo Matias <carrizomatias007@gmail.com>
2023-02-13 19:48:57 -0300

    Method GetLis and SetData with parameters

--- MatuteNotes/DataAcces/Repositories/StickyNotesRepository.cs
+++ MatuteNotes/DataAcces/Repositories/StickyNotesRepository.cs
@@ -20,14 +20,45 @@ namespace MatuteNotes.DataAcces.Repositories
             _adoContext = adoContext;
         }
 
-        public Task<IEnumerable<StickyQuery>> GetListado(StickyQueryParams @params)
+        public async Task<IEnumerable<StickyQuery>> GetListado(StickyQueryParams @params)
         {
-            throw new NotImplementedException();
+            string sqlString = "UspStickyNoteSearch";
+            var response = await _adoContext.ExecuteReader<StickyQuery>(
+                sqlString,
+                System.Data.CommandType.StoredProcedure,
+                new System.Data.SqlClient.SqlParameter[]
+                {
+                    new System.Data.SqlClient.SqlParameter("@Completed", @params.Completed),
+                    new System.Data.SqlClient.SqlParameter("@IdUser", @params.IdUser),
+                    new System.Data.SqlClient.SqlParameter("@FilterDate", @params.FilterDate),
+                    new System.Data.SqlClient.SqlParameter("@StartDate", @params.StarDate),
+                    new System.Data.SqlClient.SqlParameter("@EndDate", @params.EndDate)
+
+
+                });
+            return response;
         }
 
-        public Task<int> SetData(StickyNote note, string acccion)
+        public async Task<int> SetData(StickyNote note, string acccion)
         {
-            throw new NotImplementedException();
+            string sqlString = "UspStickyNoteMerge";
+            var response = await _adoContext.ExecuteNonQuery(
+                sqlString,
+                System.Data.CommandType.StoredProcedure,
+                new System.Data.SqlClient.SqlParameter[]
+                {
+                    new System.Data.SqlClient.SqlParameter("@Id",note.Id ),
+                    new System.Data.SqlClient.SqlParameter("@Cliente", note.Cliente),
+                    new System.Data.SqlClient.SqlParameter("@Content", note.Content),
+                    new System.Data.SqlClient.SqlParameter("@Completed", note.Completed),
+                    new System.Data.SqlClient.SqlParameter("@IdUser", note.IdUser),
+                    new System.Data.SqlClient.SqlParameter("@Priority", note.Priority),
+                    new System.Data.SqlClient.SqlParameter("@Deleted", note.Deleted),
+                    new System.Data.SqlClient.SqlParameter("@Height", note.Height),
+                    new System.Data.SqlClient.SqlParameter("@action", acccion)
+
+                });
+            return response;
         }
     }
 }
