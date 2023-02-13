05e2885c032f8aee0ced59028610521753e03bcc
Carrizo Matias <carrizomatias007@gmail.com>
2023-02-13 19:18:30 -0300

    UnitOfWork ya accede a la bd.

--- MatuteNotes/DataAcces/UnitOfWork.cs
+++ MatuteNotes/DataAcces/UnitOfWork.cs
@@ -1,8 +1,10 @@
-﻿using MatuteNotes.DataAcces.Repositories;
+﻿using MatuteNotes.DataAcces.AdoNet;
+using MatuteNotes.DataAcces.Repositories;
 using MatuteNotes.Domain;
 using MatuteNotes.Domain.Repositories;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +14,16 @@ namespace MatuteNotes.DataAcces
     public class UnitOfWork : IUnitOfWork
     {
 
+        AdoContext _adoContext;
+        private string adoNetUrl = ConfigurationManager.ConnectionStrings["dbase"].ConnectionString;
         public UnitOfWork()
         {
+            _adoContext = new AdoContext(adoNetUrl);
             Stickys = new StickyNotesRepository();
             Users = new UserRepository();
 
         }
 
-        //asdasdasdasd
         public IStickyNoteRepository Stickys { get; private set; } 
 
         public IUserRepository Users { get; private set; }
